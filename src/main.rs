4da988f2bfe1983021acd0e300816890b76db0a3
Thane Walkup <thanewalkup@gmail.com>
2023-02-23 22:05:53 -0800

    Graphical mode added

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@
 mod map;
 mod map_builder;
 mod player;
+mod camera;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
-    pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT:i32 = 50;
+    pub const SCREEN_WIDTH:  i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT:i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
-    pub use crate::map_builder::*;
     pub use crate::player::*;
-    pub const MOVE_UP: Point = Point { x: 0, y: -1};
-    pub const MOVE_DOWN: Point = Point { x: 0, y: 1};
-    pub const MOVE_RIGHT: Point = Point { x: 1, y: 0};
-    pub const MOVE_LEFT: Point = Point { x: -1, y: 0};
+    pub use crate::map_builder::*;
+    pub use crate::camera::*;
+    pub const MOVE_UP:        Point = Point { x:  0, y: -1};
+    pub const MOVE_UPLEFT:    Point = Point { x: -1, y: -1};
+    pub const MOVE_UPRIGHT:   Point = Point { x:  1, y: -1};
+    pub const MOVE_DOWN:      Point = Point { x:  0, y:  1};
+    pub const MOVE_DOWNRIGHT: Point = Point { x:  1, y:  1};
+    pub const MOVE_DOWNLEFT:  Point = Point { x: -1, y:  0};
+    pub const MOVE_RIGHT:     Point = Point { x:  1, y:  0};
+    pub const MOVE_LEFT:      Point = Point { x: -1, y:  0};
 }
 
 use prelude::*;
@@ -22,6 +30,7 @@ use prelude::*;
 struct State {
     map: Map,
     player: Player,
+    camera: Camera
 }
 
 impl State {
@@ -29,27 +38,39 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         Self { 
-            map: map_builder.map,
+            map : map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start)
         }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
 
     }
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
     .with_title("Rusty Rogue")
     .with_fps_cap(30.0)
+    .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    .with_tile_dimensions(32, 32)
+    .with_resource_path("resources/")
+    .with_font("dungeonfont.png", 32, 32)
+    .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+    .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, 
+        "dungeonfont.png")
     .build()?;
 
     main_loop(context, State::new())
 }
+
--- src/map_builder.rs
+++ src/map_builder.rs
@@ -11,10 +11,11 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+
         let mut mb = MapBuilder{
             map : Map::new(),
             rooms : Vec::new(),
-            player_start : Point::zero()
+            player_start : Point::zero(),
         };
         mb.fill(TileType::Wall);
         mb.build_random_rooms(rng);
@@ -32,8 +33,8 @@ impl MapBuilder {
             let room = Rect::with_size(
                 rng.range(1, SCREEN_WIDTH - 10),
                 rng.range(1, SCREEN_HEIGHT - 10),
-                rng.range(2,10),
-                rng.range(2,10),
+                rng.range(2, 10),
+                rng.range(2, 10),
             );
             let mut overlap = false;
             for r in self.rooms.iter() {
@@ -43,13 +44,24 @@ impl MapBuilder {
             }
             if !overlap {
                 room.for_each(|p| {
-                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0
+                    && p.y < SCREEN_HEIGHT
                     {
-                        let idx  = map_idx(p.x, p.y);
+                        let idx = map_idx(p.x, p.y);
                         self.map.tiles[idx] = TileType::Floor;
                     }
                 });
+
                 self.rooms.push(room)
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1:i32, x2:i32, y:i32) {
+        use std::cmp::{min, max};
+        for x in min(x1,x2) ..= max(x1,x2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
             }
         }
     }
@@ -57,19 +69,8 @@ impl MapBuilder {
     fn apply_vertical_tunnel(&mut self, y1:i32, y2:i32, x:i32) {
         use std::cmp::{min, max};
         for y in min(y1,y2) ..= max(y1,y2) {
-            if let Some(idx) = self.map.try_idx(Point::new(x,y)) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
                 self.map.tiles[idx as usize] = TileType::Floor;
-            }
-        }
-    }
-
-    fn apply_horizontal_tunnel(&mut self, x1:i32, x2:i32,y:i32) {
-        use std::cmp::{min,max};
-        for x in min(x1,x2) ..= max(x1,x2) {
-            if let Some(idx) = self.map.try_idx(Point::new(x,y)) {
-                if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
-                    self.map.tiles[idx as usize] = TileType::Floor;
-                }
             }
         }
     }
@@ -83,7 +84,7 @@ impl MapBuilder {
             let new = room.center();
 
             if rng.range(0,2) == 1 {
-                self.apply_horizontal_tunnel(prev.x,new.x,prev.y);
+                self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
                 self.apply_vertical_tunnel(prev.y, new.y, new.x);
             } else {
                 self.apply_vertical_tunnel(prev.y, new.y, prev.x);
