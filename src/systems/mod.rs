13379c7a3bf0564d05b0dd0ad6046b39737705c4
Thane Walkup <thanewalkup@gmail.com>
2023-02-26 05:35:54 -0800

    Implemented message-based movement.

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -9,6 +9,7 @@ mod map_render;
 mod entity_render;
 mod player_input;
 mod random_move;
+mod movement;
 mod end_turn;
 
 pub fn build_input_scheduler() -> Schedule {
@@ -22,6 +23,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
@@ -32,6 +35,8 @@ pub fn build_player_scheduler() -> Schedule {
 
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(random_move::random_move_system())
         .flush()
         .add_system(collisions::collisions_system())
