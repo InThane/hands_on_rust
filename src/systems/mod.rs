4c447ea08c29e0a70ac75f52703678c76bd90a74
Thane Walkup <thanewalkup@gmail.com>
2023-02-25 20:17:36 -0800

    collisions added

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@
 
 use crate::prelude::*;
 
+mod collisions;
 mod map_render;
 mod entity_render;
 mod player_input;
@@ -9,6 +10,7 @@ mod player_input;
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collisions_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
